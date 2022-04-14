035a8fb47914e487ba971c8105ed9ce51a8469c3
Nathan Tugwell <ntugwell@petsathome.co.uk>
2022-04-14 09:27:09 +0100

    Initial Commit

---
+++ StoreBooking/StoreBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreBookingApp.swift
+//  StoreBooking
+//
+//  Created by Nathan Tugwell on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct StoreBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
